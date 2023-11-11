77b241d66b360832dbec9ea71a26872ceced3eb3
touhidcodes <touhidcodes@gmail.com>
2023-11-12 01:27:12 +0600

    encapsulation

---
+++ src/3.10-encapsulation.ts
@@ -0,0 +1,40 @@
+{
+  //  Encapsulation
+  //  encapsulated property is only available for child class
+  class BankAccount {
+    readonly id: number;
+    public name: string;
+    private _balance: number;
+    protected _protectedBalance: number;
+    constructor(
+      id: number,
+      name: string,
+      _balance: number,
+      _protectedBalance: number
+    ) {
+      this.id = id;
+      this.name = name;
+      this._balance = _balance;
+      this._protectedBalance = _protectedBalance;
+    }
+    protected addDeposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
+    protected getBalance() {
+      return this._balance;
+    }
+    getProtectedMethod() {
+      return this.getBalance();
+    }
+  }
+
+  class studentAccount extends BankAccount {
+    test() {
+      this._protectedBalance;
+    }
+  }
+
+  const bankAccount1 = new BankAccount(111, "Mr. Banker", 200, 0);
+  bankAccount1.addDeposit(200);
+  //
+}
